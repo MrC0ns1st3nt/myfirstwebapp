86d001a9180cfe7c4c56ecd709406ae72589fc03
MrC0ns1st3nt <55199458+MrC0ns1st3nt@users.noreply.github.com>
2026-09-25 16:24:49 +0100

    Updated files for Week 4
    Updated layout and added new elements, update CSS to accommodate the changes, and added JS as requested.

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('mission-form');
+const status = document.getElementById('transmission-status');
+const statusDetail = document.getElementById('status-detail');
+const resetBtn = document.getElementById('status-reset');
+const missionPanel = document.getElementById('mission-panel');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  statusDetail.innerHTML = `TO: STARFLEET COMMAND`;
+
+  missionPanel.classList.add('is-locked');
+  form.disabled = true;
+});
+
+resetBtn.addEventListener('click', () => {
+  form.reset();
+  form.disabled = false;
+  missionPanel.classList.remove('is-locked');
+});
